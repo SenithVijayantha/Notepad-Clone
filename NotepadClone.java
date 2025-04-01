71333067555fa7adfbb4c4763e81ebe01cbe3b2d
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-04-01 19:19:12 +0530

    Add text bold and text italic functionalities

--- NotepadClone.java
+++ NotepadClone.java
@@ -1,11 +1,15 @@
+import org.w3c.dom.Attr;
+
 import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 
 public class NotepadClone extends JFrame {
-    private JTextArea textArea;
-    private JFileChooser fileChooser;
+    final private JTextPane textPane;
+    final private JFileChooser fileChooser;
 
     public NotepadClone() {
 
@@ -17,17 +21,27 @@ public class NotepadClone extends JFrame {
         setLocationRelativeTo(null); // Center the window
 
 //        Components
-        textArea = new JTextArea();
+        textPane = new JTextPane();
         fileChooser = new JFileChooser();
+        
+//        Change the font
+        Font textAreaFont = new Font("Consolas", Font.PLAIN, 14);
+        textPane.setFont(textAreaFont);
+
+//        Change the color of text pane
+        textPane.setBackground(Color.BLUE);
+        textPane.setForeground(Color.YELLOW);
 
 //        Add text area with scroll functionality
-        JScrollPane scrollPane = new JScrollPane(textArea);
+        JScrollPane scrollPane = new JScrollPane(textPane);
         add(scrollPane, BorderLayout.CENTER);
 
 //        Create Menu bar
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
+        JMenu textStyles = new JMenu("Style");
         menuBar.add(fileMenu);
+        menuBar.add(textStyles);
 
 //        Create Menu items
         JMenuItem newMenuItem = new JMenuItem("New");
@@ -35,15 +49,60 @@ public class NotepadClone extends JFrame {
         JMenuItem saveMenuItem = new JMenuItem("Save");
         JMenuItem exitMenuItem = new JMenuItem("Exit");
 
+        JMenuItem boldTextItem = new JMenuItem("<html><b>Bold</b></html>");
+        JMenuItem italicTextItem = new JMenuItem("<html><i>Italic</i></html>");
+        JMenuItem underlineTextItem = new JMenuItem("<html><u>Underline</u></html>");
+
         fileMenu.add(newMenuItem);
         fileMenu.add(openMenuItem);
         fileMenu.add(saveMenuItem);
         fileMenu.addSeparator();
         fileMenu.add(exitMenuItem);
 
+        textStyles.add(boldTextItem);
+        textStyles.add(italicTextItem);
+        textStyles.add(underlineTextItem);
+
 //        Add menu bar to frame
         setJMenuBar(menuBar);
 
+//        Actions listeners
+        boldTextItem.addActionListener(e -> makeSelectedTextBold());
+        italicTextItem.addActionListener(e -> makeSelectedTextItalic());
+    }
+
+    private void makeSelectedTextItalic() {
+        StyledDocument doc = textPane.getStyledDocument();
+        int start = textPane.getSelectionStart();
+        int end = textPane.getSelectionEnd();
+
+        if (start != end) { // Checks whether there's a selection or not
+            AttributeSet attributes = doc.getCharacterElement(start).getAttributes();
+            boolean isItalic = StyleConstants.isItalic(attributes); // Check actual bold state
+            SimpleAttributeSet italic = new SimpleAttributeSet();
+
+            StyleConstants.setItalic(italic, !isItalic);
+
+            doc.setCharacterAttributes(start, end - start, italic, false);
+        }
+
+
+    }
+
+    private void makeSelectedTextBold() {
+        StyledDocument doc = textPane.getStyledDocument();
+        int start = textPane.getSelectionStart();
+        int end = textPane.getSelectionEnd();
+
+        if (start != end) { // Checks whether there's a selection or not
+            AttributeSet attributes = doc.getCharacterElement(start).getAttributes();
+            boolean isBold = StyleConstants.isBold(attributes); // Check actual bold state
+            SimpleAttributeSet bold = new SimpleAttributeSet();
+
+            StyleConstants.setBold(bold, !isBold);
+
+            doc.setCharacterAttributes(start, end - start, bold, false);
+        }
     }
 
     public static void main(String[] args) {
